5118588c91fe32dccaf827c6de38c033cc90697c
Pindikura Ravindra <ravindra@dremio.com>
2019-01-14 23:25:54 +0530

    ARROW-4274: split decimal into two parts

--- cpp/src/gandiva/literal_holder.h
+++ cpp/src/gandiva/literal_holder.h
@@ -23,13 +23,13 @@
 #include <boost/variant.hpp>
 
 #include <arrow/type.h>
-#include "gandiva/decimal_full.h"
+#include "gandiva/decimal_scalar.h"
 
 namespace gandiva {
 
 using LiteralHolder =
     boost::variant<bool, float, double, int8_t, int16_t, int32_t, int64_t, uint8_t,
-                   uint16_t, uint32_t, uint64_t, std::string, Decimal128Full>;
+                   uint16_t, uint32_t, uint64_t, std::string, DecimalScalar128>;
 
 }  // namespace gandiva
 
